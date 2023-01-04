ead0eab10b6f4b4154cb7d636996527f99e52655
olawale afuye <walosha@yahoo.com>
2023-01-04 12:18:04 +0100

    fix

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='print_hello_afuye',
+      version='0.1',
+      description='The print hello function that accept a messg',
+      url='https://github.com/walosha/print_hello',
+      author='Olawale Afuye',
+      author_email='walosha@yahoo.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
